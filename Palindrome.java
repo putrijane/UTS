835bb0933501c9968f232fb8b9e5dd88273e6c49
putri jane <79836112+putrijane@users.noreply.github.com>
2021-04-20 14:15:01 +0700

    Add files via upload

--- Palindrome.java
+++ Palindrome.java
@@ -1,22 +1,32 @@
 import java.util.Scanner;
+import java.util.Stack;
 public class Palindrome {
-    
-    public static void main(String[] args){
-		
-	}
-	
-	private void Palindrome(String teks){		
-		String poli = "";		
 
-		for(int i=teks.length();i>0;i--){
-			String test1 = teks.substring(i-1, i);
-			poli += test1;
-		}
-		
-		if(teks.equals(poli)){
-			System.out.println("==== "+teks+" adalah Palindrome =====");
-		}else{
-			System.out.println("==== "+teks+" bukan Palindrome =====");
-		}
-	}
+    public static void main(String[] args) 
+    {
+        System.out.print("Masukkan Kata : ");
+        Scanner in = new Scanner(System.in);
+        String origString = in.nextLine();
+ 
+        Stack<Character> stack = new Stack<>();
+ 
+        //Push all chars in stack
+        for (int i = 0; i < origString.length(); i++) {
+            stack.push(origString.charAt(i));
+        }
+ 
+        String reverseString = "";
+ 
+        //Pop all chars from stack one by one and build reverse string
+        while (!stack.isEmpty()) {
+            reverseString = reverseString + stack.pop();
+        }
+ 
+        //Check palindrome string
+        if (origString.equals(reverseString)) {
+            System.out.println("Kata adalah palindrome.");
+        } else {
+            System.out.println("Kata Bukan palindrome.");
+        }
+    }
 }
